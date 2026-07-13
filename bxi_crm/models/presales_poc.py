ca28330e04a4da6765326d1c9f54ae40fbf6d887
Rohit Chaudhary <rohit.chaudhary@bxitech.com>
2026-07-13 15:51:11 +0530

    bxi enterprise production code

--- bxi_crm/models/presales_poc.py
+++ bxi_crm/models/presales_poc.py
@@ -14,3 +14,10 @@ class PresalesPOC(models.Model):
     employee_id = fields.Many2one('hr.employee',
         string="Employee",
     )
+
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        if self.employee_id:
+            self.name = self.employee_id.name
+
